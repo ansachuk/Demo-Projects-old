ab44a8d699b46c9aecc63e506ce86aad49b39218
ansachuk <ansachuk@icloud.com>
2023-04-07 20:11:36 +0300

    make universal markup and js

--- src/scripts/progress-steps.js
+++ src/scripts/progress-steps.js
@@ -1,27 +1,15 @@
 const stepsRef = document.querySelectorAll(".steps__item");
-const nextButtonsRef = document.querySelector("[data-action='next']");
 const prevButtonsRef = document.querySelector("[data-action='prev']");
+const nextButtonsRef = document.querySelector("[data-action='next']");
 
 const ACTIVE_CLASS = "steps_active";
 
 let currentActiveStepIndex = 0;
 
-nextButtonsRef.addEventListener("click", nextStepClickHandler);
-prevButtonsRef.addEventListener("click", prevStepClickHandler);
+prevButtonsRef.addEventListener("click", prevStepButtonClickHandler);
+nextButtonsRef.addEventListener("click", nextStepButtonClickHandler);
 
-function nextStepClickHandler() {
-  prevButtonsRef.removeAttribute("disabled");
-
-  currentActiveStepIndex += 1;
-  stepsRef[currentActiveStepIndex].classList.add(ACTIVE_CLASS);
-
-  if (currentActiveStepIndex === 3) {
-    nextButtonsRef.setAttribute("disabled", "true");
-    return;
-  }
-}
-
-function prevStepClickHandler() {
+function prevStepButtonClickHandler() {
   nextButtonsRef.removeAttribute("disabled");
 
   stepsRef[currentActiveStepIndex].classList.remove(ACTIVE_CLASS);
@@ -29,6 +17,18 @@ function prevStepClickHandler() {
 
   if (currentActiveStepIndex === 0) {
     prevButtonsRef.setAttribute("disabled", "true");
+    return;
+  }
+}
+
+function nextStepButtonClickHandler() {
+  prevButtonsRef.removeAttribute("disabled");
+
+  currentActiveStepIndex += 1;
+  stepsRef[currentActiveStepIndex].classList.add(ACTIVE_CLASS);
+
+  if (currentActiveStepIndex === stepsRef.length - 1) {
+    nextButtonsRef.setAttribute("disabled", "true");
     return;
   }
 }
