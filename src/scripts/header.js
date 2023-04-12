6951183d26ba3386c0aac2eff58c707d1fce156e
ansachuk <ansachuk@icloud.com>
2023-04-12 19:33:04 +0300

    finish styling checkbox

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,2 +1,8 @@
 import "modern-normalize/modern-normalize.css";
 import "../sass/partials/_header.scss";
+
+const checkbox = document.querySelector("#theme-mode-checkbox");
+
+checkbox.addEventListener("click", e => {
+  console.log(e.currentTarget.checked);
+});
