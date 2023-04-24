cf92c71c4694d2f0a52f6c9bb7f2f8c2a5b1ca0d
ansachuk <ansachuk@icloud.com>
2023-04-24 14:54:01 +0300

    1

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,8 +1,2 @@
 import "modern-normalize/modern-normalize.css";
 import "../sass/partials/_header.scss";
-
-const checkbox = document.querySelector("#theme-mode-checkbox");
-
-checkbox.addEventListener("click", e => {
-  console.log(e.currentTarget.checked);
-});
